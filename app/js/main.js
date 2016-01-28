54b05972ce6f4341c0e2ef8474803edf0d424feb
Sanjay Rajashekhar <91sanjay@gmail.com>
2016-01-28 14:47:36 -0500

    feat: banner

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$(document).ready(function () {
+    $(function () {
+        $(".element").typed({
+            strings: ["Java", "Python", "Spring", "SQL", "HTML", "CSS", "JavaScript", "JQuery", "AngularJS", "NodeJS",
+                "Bootstrap", "Grunt", "Bower"],
+            typeSpeed: 100
+        });
+    });
+});
