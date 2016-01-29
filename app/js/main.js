d2a8bd1a48883520b1ae23dcbec38dcb370077da
Sanjay Rajashekhar <91sanjay@gmail.com>
2016-01-28 21:28:27 -0500

    chore: minor changes to svg and fixing responsive behabior of banner

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,7 @@
 $(document).ready(function () {
     $(function () {
         $(".element").typed({
-            strings: ["Java", "Python", "Spring", "SQL", "HTML", "CSS", "JavaScript", "JQuery", "AngularJS", "NodeJS",
+            strings: ["Java", "Python", "Spring", "SQL", "HTML", "CSS", "JavaScript", "JQuery", "AngularJS", "Node.js",
                 "Bootstrap", "Grunt", "Bower"],
             typeSpeed: 100
         });
