9345ba23539f2d5708566d4a7503e24f25237edc
Sanjay Rajashekhar <91sanjay@gmail.com>
2016-02-06 14:54:34 -0800

    docs: publish trial

---
+++ js/main.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready(function(){$(function(){$(".element").typed({strings:["Java","Python","Spring","SQL","HTML","CSS","JavaScript","JQuery","AngularJS","Node.js","Bootstrap","Grunt","Bower"],typeSpeed:100})}),$('[data-toggle="tooltip"]').tooltip({placement:"top"})});
